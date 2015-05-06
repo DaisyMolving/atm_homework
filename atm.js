95008e2698a81b438ae0ed16e78950dd36fb47dd
Daisy Molving <daisymolving@gmail.com>
2015-05-07 06:49:18 +1200

    declared variable firstInput to try to store the result of the deposit (balance). used the balance to minus the 2nd input from. did not work

--- atm.js
+++ atm.js
@@ -1,25 +1,36 @@
-var inputAmountReg = [];
+$(document).ready(function(){ 
+
+var firstInput = [];
 var regAccountDisplay = [];
 var inputAmountSave = [];
 var saveAccountDisplay = [];
 
 
-$(document).ready(function(){ 
 
-  $('#deposit1').on('click', function(){
+
+
+firstInput = $('#deposit1').on('click', function(){
     // $('#amount1').val();
-    $('#balance1').html("$" + $('#amount1').val());
+  $('#balance1').html("$" + $('#amount1').val());
   });
-
 
   $('#deposit2').on('click', function(){
     $('#balance2').html("$" + $('#amount2').val());
   });
 
 
-  
 
-}) 
+
+  $('#withdraw1').on('click', function(){
+    console.log(firstInput);
+  });
+
+   $('#withdraw2').on('click', function(){
+    console.log(Number($('#amount2')[0]) - Number($('#amount2')[1]));
+  });
+
+
+}); 
 
 
 
