8f7b08e11bfc07d4551c4f4f3452cb41edadcea3
Daisy Molving <daisymolving@gmail.com>
2015-05-07 07:29:03 +1200

    made if else statement to redefine #balance in html if it was not 0.

--- atm.js
+++ atm.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){ 
 
-var firstInput = [];
+var firstInput = 0;
+var secondInput = 0;
 var regAccountDisplay = [];
 var inputAmountSave = [];
 var saveAccountDisplay = [];
 
 
 
-
-
-firstInput = $('#deposit1').on('click', function(){
+$('#deposit1').on('click', function(){
     // $('#amount1').val();
-  $('#balance1').html("$" + $('#amount1').val());
+  $('#balance1').html($('#amount1').val());
+  firstInput = Number($('#balance1').text());
   });
 
   $('#deposit2').on('click', function(){
@@ -22,7 +22,10 @@ firstInput = $('#deposit1').on('click', function(){
 
 
   $('#withdraw1').on('click', function(){
-    console.log(firstInput);
+  if (firstInput === "0") {
+    console.log("oops");
+  } else {secondInput = Number($('#balance1'));
+console.log(secondInput - firstInput);}
   });
 
    $('#withdraw2').on('click', function(){
