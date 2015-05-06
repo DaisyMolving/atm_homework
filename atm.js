0737f7439a079382f1bd35a275c2715cdffc8c0a
Daisy Molving <daisymolving@gmail.com>
2015-05-07 06:01:18 +1200

    got lost and started again. made empty variables for both inputs and both displays. made function for deposit buttons for both accounts, which take the value from input and display it by inputting value into html file

--- atm.js
+++ atm.js
@@ -1,15 +1,55 @@
-$(document).ready(function(){
+var inputAmountReg = [];
+var regAccountDisplay = [];
+var inputAmountSave = [];
+var saveAccountDisplay = [];
+
+
+$(document).ready(function(){ 
+
+  $('#deposit1').on('click', function(){
+    // $('#amount1').val();
+    $('#balance1').html("$" + $('#amount1').val());
+  });
+
+
+  $('#deposit2').on('click', function(){
+    $('#balance2').html("$" + $('#amount2').val());
+  });
+
+
   
-})
+
+}) 
 
 
-var inputAmount = function() {
-  $('input#amount1').val();
-};
 
-var depositAmount = function(event) {
-  $('input#deposit1').on('click', inputAmount).display($('div#balance1'));
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
